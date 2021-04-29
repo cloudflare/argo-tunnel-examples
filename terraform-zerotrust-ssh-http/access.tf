8e7d3f067656b78d2d6639e2d8537d5571e66048
Chris De La Garza <cdelagarza@cloudflare.com>
2021-04-29 11:57:43 -0500

    Initial commit for terraform demo

---
+++ terraform-zerotrust-ssh-http/access.tf
@@ -0,0 +1,19 @@
+# Access policy to apply zero trust policy over SSH endpoint
+resource "cloudflare_access_application" "ssh_app" {
+  zone_id          = var.cloudflare_zone_id
+  name             = "Access protection for ssh.${var.cloudflare_zone}"
+  domain           = "ssh.${var.cloudflare_zone}"
+  session_duration = "1h"
+}
+
+resource "cloudflare_access_policy" "ssh_policy" {
+  application_id = cloudflare_access_application.ssh_app.id
+  zone_id        = var.cloudflare_zone_id
+  name           = "Example Policy for ssh.${var.cloudflare_zone}"
+  precedence     = "1"
+  decision       = "allow"
+
+  include {
+    email = [var.cloudflare_email]
+  }
+}
